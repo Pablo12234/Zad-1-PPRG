8155e9baa9338280ad00211b14a20739235c675e
Pablo12234 <116223409+Pablo12234@users.noreply.github.com>
2022-10-20 00:47:05 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Sprawdz czy liczb jest parzysta czy nieporzesta"<< endl;
+    cout<<"Podaj liczbe" ;
+    
+    int liczba;
+    cin>>liczba;
+    
+    if (liczba == 0)
+    cout << "liczba jest nieparzysta";
+    
+    else if (liczba%2 == 0)
+    cout <<"Liczba jest parzysta";
+    
+    else 
+    cout <<"Liczba jest nieparzysta";
+
+    return 0;
+}
